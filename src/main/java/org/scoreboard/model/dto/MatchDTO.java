de795511dad6cddb5b61594bb4a1cc767e8392f5
Kubilay ÇAKIR <kubilaycakirr52@gmail.com>
2023-01-22 16:50:49 +0300

    The necessary models were created. (DTOs, Entities, Requests and Mappers)

--- src/main/java/org/scoreboard/model/dto/MatchDTO.java
+++ src/main/java/org/scoreboard/model/dto/MatchDTO.java
@@ -1,4 +1,106 @@
 package org.scoreboard.model.dto;
 
+import org.scoreboard.model.enums.MatchStatus;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class MatchDTO {
+
+    private UUID id;
+    private String homeTeam;
+    private String awayTeam;
+    private Integer homeTeamScore;
+    private Integer awayTeamScore;
+    private Date matchDate;
+    private Date matchStartDate;
+    private Date MatchEndDate;
+    private MatchStatus matchStatus;
+
+    public MatchDTO() {
+    }
+
+    public MatchDTO(UUID id, String homeTeam, String awayTeam, Integer homeTeamScore, Integer awayTeamScore, Date matchDate, Date matchStartDate, Date matchEndDate, MatchStatus matchStatus) {
+        this.id = id;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamScore = homeTeamScore;
+        this.awayTeamScore = awayTeamScore;
+        this.matchDate = matchDate;
+        this.matchStartDate = matchStartDate;
+        this.MatchEndDate = matchEndDate;
+        this.matchStatus = matchStatus;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public Integer getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public void setHomeTeamScore(Integer homeTeamScore) {
+        this.homeTeamScore = homeTeamScore;
+    }
+
+    public Integer getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+    public void setAwayTeamScore(Integer awayTeamScore) {
+        this.awayTeamScore = awayTeamScore;
+    }
+
+    public Date getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(Date matchDate) {
+        this.matchDate = matchDate;
+    }
+
+    public Date getMatchStartDate() {
+        return matchStartDate;
+    }
+
+    public void setMatchStartDate(Date matchStartDate) {
+        this.matchStartDate = matchStartDate;
+    }
+
+    public Date getMatchEndDate() {
+        return MatchEndDate;
+    }
+
+    public void setMatchEndDate(Date matchEndDate) {
+        MatchEndDate = matchEndDate;
+    }
+
+    public MatchStatus getMatchStatus() {
+        return matchStatus;
+    }
+
+    public void setMatchStatus(MatchStatus matchStatus) {
+        this.matchStatus = matchStatus;
+    }
 }
--- src/main/java/org/scoreboard/model/request/AddMatchRequest.java
+++ src/main/java/org/scoreboard/model/request/AddMatchRequest.java
@@ -1,4 +1,43 @@
 package org.scoreboard.model.request;
 
+import java.util.Date;
+
 public class AddMatchRequest {
+
+    private String homeTeam;
+    private String awayTeam;
+    private Date matchDate;
+
+    public AddMatchRequest() {
+    }
+
+    public AddMatchRequest(String homeTeam, String awayTeam, Date matchDate) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.matchDate = matchDate;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public Date getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(Date matchDate) {
+        this.matchDate = matchDate;
+    }
 }
