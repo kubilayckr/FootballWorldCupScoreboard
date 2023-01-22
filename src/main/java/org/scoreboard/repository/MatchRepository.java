cd56df8d66afc7aedaffad619d580f3a851022f5
Kubilay ÇAKIR <kubilaycakirr52@gmail.com>
2023-01-22 18:41:10 +0300

    I wrote the necessary methods for CRUD operations.

--- src/main/java/org/scoreboard/repository/MatchRepository.java
+++ src/main/java/org/scoreboard/repository/MatchRepository.java
@@ -4,9 +4,7 @@ import org.scoreboard.mapper.MatchMapper;
 import org.scoreboard.model.entity.Match;
 import org.scoreboard.util.DBUtil;
 
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class MatchRepository {
 
@@ -18,21 +16,32 @@ public class MatchRepository {
     }
 
     public Match addMatch(Match match) {
-        return null;
+        match.setId(UUID.randomUUID());
+        match.setVersion(1);
+        tableMatch.put(match.getId(), match);
+        return match;
     }
 
     public Match updateMatch(Match match) {
-        return null;
+        Match existingMatch = tableMatch.get(match.getId());
+        mapper.update(existingMatch, match);
+        existingMatch.setVersion(existingMatch.getVersion()+1);
+        return existingMatch;
     }
 
     public void deleteMatch(Match match) {
+        tableMatch.remove(match.getId());
     }
 
     public Match getById(UUID id) {
-        return null;
+        return tableMatch.get(id);
     }
 
     public List<Match> getAll() {
-        return null;
+        List<Match> allMatches = new ArrayList<>();
+        for (Map.Entry<UUID, Match> entry : tableMatch.entrySet()) {
+            allMatches.add(entry.getValue());
+        }
+        return allMatches;
     }
 }
